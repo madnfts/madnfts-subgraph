8a3748c5f300734199fce25eb621b3ce8f198578
devhallsmad <mark@madinart.io>
2022-10-16 14:02:17 +0100

    1155 amounts integration

--- src/create/account.ts
+++ src/create/account.ts
@@ -1,8 +1,11 @@
 import { Address } from '@graphprotocol/graph-ts/index';
 import { Account } from '../../generated/schema';
+import { decimal } from '@protofire/subgraph-toolkit/index';
 
 export function createAccount(address: Address): Account {
   let account = new Account(address)
+  account.volume = 0
+  account.volumePrice = decimal.ZERO
   account.save()
   return account
 }
--- src/create/order.ts
+++ src/create/order.ts
@@ -1,40 +1,45 @@
 import { Address, BigInt, Bytes } from '@graphprotocol/graph-ts/index';
-import { Bid, Order, Sale } from '../../generated/schema';
+import { Account, Bid, Order, Sale } from '../../generated/schema';
 import { fetchERC1155Token, fetchERC721Token } from '../fetch/token';
 import { fetchERC1155, fetchERC721 } from '../fetch/factory';
 import { fetchAccount } from '../fetch/account';
 import { fetchOrder } from '../fetch/order';
 import {
   ERC721Marketplace,
-  ERC721Marketplace__orderInfoResult as OrderInfo,
+  ERC721Marketplace__orderInfoResult as OrderInfo721,
 } from '../../generated/ERC721Marketplace/ERC721Marketplace';
 import { ethereum } from '@graphprotocol/graph-ts';
 import { integer, decimal, DEFAULT_DECIMALS, ZERO_ADDRESS } from '@protofire/subgraph-toolkit'
+import {
+  ERC1155Marketplace,
+  ERC1155Marketplace__orderInfoResult as OrderInfo1155
+} from '../../generated/ERC1155Marketplace/ERC1155Marketplace';
 
 export function createOrder(
   tokenStandard: String,
   orderHash: Bytes,
   tokenId: BigInt,
+  quantity: BigInt | null,
   sellerAddress: Address,
   contractAddress: Address,
   marketplaceAddress: Address,
   block: ethereum.Block
 ): Order {
-  let marketplace721interface = ERC721Marketplace.bind(Address.fromBytes(marketplaceAddress))
-  let try_orderInfo = marketplace721interface.try_orderInfo(orderHash)
-  let orderInfo: OrderInfo = try_orderInfo.value
-  let order = new Order(orderHash.toHex())
-  if (orderInfo) {
-    let seller = fetchAccount(sellerAddress)
-    order.hash = orderHash
-    order.type = orderInfo.getOrderType()
-    order.endPrice = orderInfo.getEndPrice()
-    order.endTime = orderInfo.getEndTime()
-    order.startPrice = orderInfo.getStartPrice()
-    order.startTime = orderInfo.getStartTime()
-    order.canceled = false
-    order.seller = seller.id
-    if (tokenStandard == '721') {
+  if (tokenStandard == '721') {
+    let marketplace721interface = ERC721Marketplace.bind(Address.fromBytes(marketplaceAddress))
+    let try_orderInfo = marketplace721interface.try_orderInfo(orderHash)
+    let orderInfo: OrderInfo721 = try_orderInfo.value
+    let order = new Order(orderHash.toHex())
+    if (orderInfo) {
+      let seller = fetchAccount(sellerAddress)
+      order.hash = orderHash
+      order.type = orderInfo.getOrderType()
+      order.endPrice = orderInfo.getEndPrice()
+      order.endTime = orderInfo.getEndTime()
+      order.startPrice = orderInfo.getStartPrice()
+      order.startTime = orderInfo.getStartTime()
+      order.canceled = false
+      order.seller = seller.id
       let contract = fetchERC721(contractAddress)
       let token = fetchERC721Token(contract, sellerAddress, tokenId, block.timestamp)
       order.ERC721token = token.id
@@ -42,7 +47,25 @@ export function createOrder(
       token.order = order.id
       token.timestamp = block.timestamp
       token.save()
-    } else {
+    }
+    order.save()
+    return order as Order
+  } else {
+    let marketplace1155interface = ERC1155Marketplace.bind(Address.fromBytes(marketplaceAddress))
+    let try_orderInfo = marketplace1155interface.try_orderInfo(orderHash)
+    let orderInfo: OrderInfo1155 = try_orderInfo.value
+    let order = new Order(orderHash.toHex())
+    if (orderInfo) {
+      let seller = fetchAccount(sellerAddress)
+      order.hash = orderHash
+      order.type = orderInfo.getOrderType()
+      order.endPrice = orderInfo.getEndPrice()
+      order.endTime = orderInfo.getEndTime()
+      order.startPrice = orderInfo.getStartPrice()
+      order.startTime = orderInfo.getStartTime()
+      order.quantity = quantity as BigInt
+      order.canceled = false
+      order.seller = seller.id
       let contract = fetchERC1155(contractAddress)
       let token = fetchERC1155Token(contract, tokenId, null)
       order.ERC1155token = token.id
@@ -52,8 +75,8 @@ export function createOrder(
       token.save()
     }
     order.save()
+    return order as Order
   }
-  return order as Order
 }
 
 export function createSale(
@@ -92,6 +115,10 @@ export function createSale(
     contract.volume = (contract.volume > 0 ? contract.volume + 1 : 1)
     contract.volumePrice = contract.volumePrice + amount
     contract.save()
+    let ownerAccount = Account.load(contract.owner as Bytes) as Account
+    ownerAccount.volume = (ownerAccount.volume > 0 ? ownerAccount.volume + 1 : 1)
+    ownerAccount.volumePrice = ownerAccount.volumePrice + amount
+    ownerAccount.save()
   } else {
     let contract = fetchERC1155(contractAddress)
     let token = fetchERC1155Token(contract, tokenId, null)
@@ -108,6 +135,10 @@ export function createSale(
     contract.volume = (contract.volume > 0 ? contract.volume + 1 : 1)
     contract.volumePrice = contract.volumePrice + amount
     contract.save()
+    let ownerAccount = Account.load(contract.owner as Bytes) as Account
+    ownerAccount.volume = (ownerAccount.volume > 0 ? ownerAccount.volume + 1 : 1)
+    ownerAccount.volumePrice = ownerAccount.volumePrice + amount
+    ownerAccount.save()
   }
   return sale as Sale
 }
