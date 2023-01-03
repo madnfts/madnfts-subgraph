dd46bdc6f73d9b37500791912472366d147aa06d
Mark Hall <mark@pendulumcreative.uk>
2023-01-03 16:36:59 +0000

    - Fixed base uri update events

--- src/contract.ts
+++ src/contract.ts
@@ -7,6 +7,7 @@ import {
   PublicMintStateSet as ERC721PublicMintStateSetEvent,
   RoyaltyFeeSet as ERC721RoyaltyFeeSetEvent,
   RoyaltyRecipientSet as ERC721RoyaltyRecipientSetEvent,
+  ERC721Basic as ERC721BasicTemplate,
 } from '../generated/templates/ERC721Basic/ERC721Basic';
 
 import {
@@ -18,6 +19,7 @@ import {
   PublicMintStateSet as ERC1155PublicMintStateSetEvent,
   RoyaltyFeeSet as ERC1155RoyaltyFeeSetEvent,
   RoyaltyRecipientSet as ERC1155RoyaltyRecipientSetEvent,
+  ERC1155Basic as ERC1155BasicTemplate,
 } from '../generated/templates/ERC1155Basic/ERC1155Basic';
 
 import { fetchAccount } from './fetch/account';
@@ -26,6 +28,7 @@ import { fetchERC721Token } from './fetch/token';
 import { fetchERC721Operator } from './fetch/operator';
 import { createERC1155Operator } from './create/operator';
 import { createERC1155BatchTransfer, createERC1155SingleTransfer, createERC721Transfer } from './create/transfer';
+import { Address } from '@graphprotocol/graph-ts/index';
 
 // 721 Token events
 
@@ -49,7 +52,9 @@ export function handle721ApprovalForAll(event: ERC721ApprovalForAllEvent): void 
 
 export function handle721BaseURISet(event: ERC721BaseURISetEvent): void {
   let contract = fetchERC721(event.address)
-  contract.baseUri = event.params.newBaseURI.toString()
+  let erc721interface = ERC721BasicTemplate.bind(Address.fromBytes(event.address))
+  let try_baseUri = erc721interface.try_getBaseURI()
+  contract.baseUri = try_baseUri.value
   contract.save()
 }
 
@@ -87,7 +92,9 @@ export function handle1155ApprovalForAll(event: ERC1155ApprovalForAllEvent): voi
 
 export function handle1155BaseURISet(event: ERC1155BaseURISetEvent): void {
   let contract = fetchERC1155(event.address, event.block.timestamp)
-  contract.baseUri = event.params.newBaseURI.toString()
+  let erc1155interface = ERC1155BasicTemplate.bind(Address.fromBytes(event.address))
+  let try_baseUri = erc1155interface.try_getURI()
+  contract.baseUri = try_baseUri.value
   contract.save()
 }
 
